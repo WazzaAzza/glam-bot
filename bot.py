e93bd1ade860e800846632b422a78412860c0673
WazzaAzza <40279678+WazzaAzza@users.noreply.github.com>
2018-06-23 15:16:59 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+# Glambot by xXwazerzXx
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix='g/')
+
+bot.login(process.env.BOT_TOKEN):
